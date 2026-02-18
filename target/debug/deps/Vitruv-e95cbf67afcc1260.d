0944fe4529d0107600256f0bf7bc1d2212c4f9ed
XXNOUR <nourppc8005@gmail.com>
2026-02-18 01:44:01 +0100

    Initial commit: Vitruvius zero-knowledge file sync

---
+++ target/debug/deps/Vitruv-e95cbf67afcc1260.d
@@ -0,0 +1,5 @@
+/home/hades/Cs/Vitruvius/Vitruv/target/debug/deps/Vitruv-e95cbf67afcc1260.d: src/main.rs
+
+/home/hades/Cs/Vitruvius/Vitruv/target/debug/deps/libVitruv-e95cbf67afcc1260.rmeta: src/main.rs
+
+src/main.rs:
