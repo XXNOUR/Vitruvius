419d8466e60dfbb7a7fd98d16506217af4ca7f2c
XXNOUR <nourppc8005@gmail.com>
2026-02-20 02:29:08 +0100

    stop tracking target directory

--- target/debug/deps/Vitruv-9ff7040a8d6da8e7.d
+++
@@ -1,5 +0,0 @@
-/home/hades/Cs/Vitruvius/Vitruv/target/debug/deps/Vitruv-9ff7040a8d6da8e7.d: src/main.rs
-
-/home/hades/Cs/Vitruvius/Vitruv/target/debug/deps/libVitruv-9ff7040a8d6da8e7.rmeta: src/main.rs
-
-src/main.rs:
